5f6db6efcdf84be920c0119e1defa3ad59e3600f
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-02 23:26:34 +0200

    Update All Auth responsive

--- src/components/Auth/CheckInbox.js
+++ src/components/Auth/CheckInbox.js
@@ -1,38 +1,39 @@
 import "./CheckInbox.css";
-import logo from './../../images/logo.svg';
+import logo from "./../../images/logo.svg";
 
 function CheckInbox() {
   return (
     <div className="check-inbox">
       <div className="container">
-        <div className="left-side">
-          <img src={logo} alt="logo" className="logo" />
-          <h1>Verify Your Email</h1>
-        </div>
-
-        <div className="card">
-          <h2>Check your inbox!</h2>
-          <p>
-            We've sent 5 digit code to <strong>example@mail.com</strong>
-            <br />
-            please enter it below
-          </p>
-
-          <a href="#" className="resend">
-            Resend Code?
-          </a>
-
-          <div className="code-inputs">
-            <input maxlength="1" />
-            <input maxlength="1" />
-            <input maxlength="1" />
-            <input maxlength="1" />
-            <input maxlength="1" />
+        <div className="left">
+          <div className="logo">
+            <img src={logo} alt="logo" />
           </div>
+          <h2>Verify Your Email</h2>
+        </div>
+        <div className="right">
+          <div className="card">
+            <h2 className="card-title">Check your inbox!</h2>
+            <p>
+              We've sent 5 digit code to example@mail.com
+              <br />
+              please enter it below
+            </p>
 
-          <button className="submit-btn">Submit</button>
+            <a href="#" className="resend">
+              Resend Code?
+            </a>
 
-          <button className="edit-btn">Edit Email</button>
+            <div className="code-inputs">
+              <input maxlength="1" />
+              <input maxlength="1" />
+              <input maxlength="1" />
+              <input maxlength="1" />
+              <input maxlength="1" />
+            </div>
+            <button className="submit-btn">Submit</button>
+            <button className="edit-btn">Edit Email</button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Auth/VerifyEmail.js
+++ src/components/Auth/VerifyEmail.js
@@ -1,37 +1,39 @@
 import "./VerifyEmail.css";
-import logo from './../../images/logo.svg';
+import logo from "./../../images/logo.svg";
 function VerifyEmail() {
   return (
     <div className="verify-email">
       <div className="container">
-        <div className="left-side">
-          <img src={logo} alt="logo" className="logo" />
-          <h1>Verify Your Email</h1>
-        </div>
-
-        <div className="card">
-          <h2>Verification Code</h2>
-          <p>
-            We've sent 5 digit code to <strong>example@mail.com</strong>
-            <br />
-            please enter it below
-          </p>
-
-          <a href="#" className="resend">
-            Resend Code?
-          </a>
-
-          <div className="code-inputs">
-            <input maxlength="1" />
-            <input maxlength="1" />
-            <input maxlength="1" />
-            <input maxlength="1" />
-            <input maxlength="1" />
+        <div className="top">
+          <div className="logo">
+            <img src={logo} alt="logo" className="logo" />
           </div>
+          <h2>Verify Your Email</h2>
+        </div>
+        <div className="bottom">
+          <div className="card">
+            <h2>Verification Code</h2>
+            <p>
+              We've sent 5 digit code to <strong>example@mail.com</strong>
+              <br />
+              please enter it below
+            </p>
 
-          <button className="submit-btn">Submit</button>
+            <a href="#" className="resend">
+              Resend Code?
+            </a>
 
-          <button className="edit-btn">Edit Email</button>
+            <div className="code-inputs">
+              <input maxlength="1" />
+              <input maxlength="1" />
+              <input maxlength="1" />
+              <input maxlength="1" />
+              <input maxlength="1" />
+            </div>
+
+            <button className="submit-btn">Submit</button>
+            <button className="edit-btn">Edit Email</button>
+          </div>
         </div>
       </div>
     </div>
