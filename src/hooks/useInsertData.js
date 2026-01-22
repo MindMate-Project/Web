7c03d8f6bd71549aa0bea02fb43fe5fab5915a0f
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-01-23 00:48:32 +0200

    intial utility hooks

---
+++ src/hooks/useInsertData.js
@@ -0,0 +1,21 @@
+import baseUrl from "../API/baseURL";
+
+const useInsertDataWithImage = async (url, parmas) => {
+  try {
+    const config = {
+      headers: { "Content-Type": "multipart/form-data" },
+    };
+    const res = await baseUrl.post(url, parmas, config);
+    console.log(res.status);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const useInsertData = async (url, parmas) => {
+  const res = await baseUrl.post(url, parmas);
+  return res;
+};
+
+export { useInsertData, useInsertDataWithImage };
