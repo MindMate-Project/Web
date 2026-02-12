7128f120873519dd88b6b74db70b8a6ab440a835
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-12 21:10:35 +0200

    feat: Add dashboard route

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,40 +1,18 @@
-import { useNavigate } from 'react-router';
+import { useNavigate } from "react-router";
+
+import Dashboard from "./../../components/Dashboard/Dashboard";
+
 function HomePage() {
     const navigate = useNavigate();
     const logOut = () => {
-      localStorage.removeItem("user");
-      localStorage.removeItem("token");
-      navigate("/api/auth/login");
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        navigate("/api/auth/login");
     };
 
     return (
-      <div className="home-page">
-        <h1 style={{ color: "white" }} >Welcome to MindMate</h1>
-        {localStorage.getItem("token") ? (
-          <div>
-            <h2 style={{ color: "white" }}>
-              welcome {JSON.parse(localStorage.getItem("user")).name}!
-            </h2>
-            {<button className="hbtn" onClick={logOut}>Log Out</button>}
-          </div>
-        ) : (
-          <div>
-            <h2>Please log in or sign up to continue</h2>
-            <button
-              className="hbtn"
-              onClick={() => navigate("/api/auth/login")}
-            >
-              Go to Login
-            </button>
-            <button
-              className="hbtn"
-              onClick={() => navigate("/api/auth/signup")}
-            >
-              Go to Signup
-            </button>
-          </div>
-        )}
-      </div>
+        <Dashboard logOut={logOut} />
     );
 }
+
 export default HomePage;
