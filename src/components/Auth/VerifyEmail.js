569d4a7161717c4364b5f0a0c81f2fb76e827521
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-19 23:55:02 +0200

    feat(redux): integrate redux in reset code

--- src/components/Auth/VerifyEmail.js
+++ src/components/Auth/VerifyEmail.js
@@ -1,10 +1,12 @@
 import "./VerifyEmail.css";
-// import logo from "./../../images/logo.svg";
 import { useNavigate } from "react-router";
-import LogoHeader from "./LogoHeader";
 import AuthLayout from "./AuthLayout";
+import VerifyPasswordHook from "../../hook/auth/verify-reset-code-hook";
+import { ToastContainer } from "react-toastify";
 function VerifyEmail() {
-  const navigate = useNavigate();
+    const [code, OnChangeCode, onSubmit] = VerifyPasswordHook();
+    
+    const navigate = useNavigate();
   return (
     <AuthLayout>
     <div className="verify-email">
@@ -13,9 +15,7 @@ function VerifyEmail() {
           <div className="card">
             <h2>Verification Code</h2>
             <p>
-              We've sent 5 digit code to example@mail.com
-              <br />
-              please enter it below
+              We've sent 6 digit code to your email address, please check your inbox
             </p>
 
             <button type="button" className="resend">
@@ -23,20 +23,22 @@ function VerifyEmail() {
             </button>
 
             <div className="code-inputs">
-              <input maxlength="1" />
-              <input maxlength="1" />
-              <input maxlength="1" />
-              <input maxlength="1" />
-              <input maxlength="1" />
+              <input maxlength="1" value={code[0] || ""} onChange={(e) => OnChangeCode(e, 0)} />
+              <input maxlength="1" value={code[1] || ""} onChange={(e) => OnChangeCode(e, 1)} />
+              <input maxlength="1" value={code[2] || ""} onChange={(e) => OnChangeCode(e, 2)} />
+              <input maxlength="1" value={code[3] || ""} onChange={(e) => OnChangeCode(e, 3)} />
+              <input maxlength="1" value={code[4] || ""} onChange={(e) => OnChangeCode(e, 4)} />
+              <input maxlength="1" value={code[5] || ""} onChange={(e) => OnChangeCode(e, 5)} />
             </div>
 
-            <button className="submit-btn">Submit</button>
+            <button className="submit-btn" onClick={onSubmit}>Submit</button>
             <button className="edit-btn"
             onClick={() => navigate("/api/auth/forgot-password")}>Edit Email</button>
           </div>
         </div>
       </div>
     </div>
+    <ToastContainer />
     </AuthLayout>
   );
 }
