94fcb0eef48d5c8b3bb7677318ba59dfb54a7a4a
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-19 23:51:12 +0200

    feat(redux): integrate redux with forget passowrd page

--- src/components/Auth/ForgetPassword.js
+++ src/components/Auth/ForgetPassword.js
@@ -1,51 +1,61 @@
 import "./ForgetPassword.css";
 import { useNavigate } from "react-router";
-import LogoHeader from "./LogoHeader";
 import AuthLayout from "./AuthLayout";
+import ForgetPasswordHook from "../../hook/auth/forget-password-hook";
+import { ToastContainer } from "react-toastify";
 function ForgetPassword() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [OnChangeEmail, email, onSubmit] = ForgetPasswordHook();
 
-  return (
-    <AuthLayout>
-    <div className="fo-pass">
-      {/* <div className="logo">
+    return (
+        <AuthLayout>
+            <div className="fo-pass">
+                {/* <div className="logo">
         <LogoHeader />
       </div> */}
-      <div className="container">
+                <div className="container">
+                    <div className="right">
+                        <div className="card">
+                            <h2 className="card-title">Forgot Password?</h2>
 
-        <div className="right">
-          <div className="card">
-            <h2 className="card-title">Forgot Password?</h2>
+                            <p className="card-desc">
+                                Don’t worry! enter your email to reset the
+                                password
+                            </p>
 
-            <p className="card-desc">
-              We’ve sent a 5 digit code to example@mail.com <br />
-              please enter it below
-            </p>
+                            {/* <a className="resend">Resend Code?</a> */}
 
-            {/* <a className="resend">Resend Code?</a> */}
+                            <form>
+                                <div className="field">
+                                    <label>Enter your email</label>
+                                    <input
+                                        onChange={OnChangeEmail}
+                                        value={email}
+                                        type="email"
+                                        placeholder="@gmail.com"
+                                    />
+                                </div>
 
-            <form>
-              <div className="field">
-                <label>Enter your email</label>
-                <input type="email" placeholder="@gmail.com" />
-              </div>
+                                <button type="button" onClick={onSubmit}>
+                                    {" "}
+                                    Send Code{" "}
+                                </button>
 
-              <button type="submit"> Send Code </button>
-
-              <button
-                type="button"
-                className="back-btn"
-                onClick={() => navigate("/api/auth/login")}
-              >
-                Back To Login
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </AuthLayout>
-  );
+                                <button
+                                    type="button"
+                                    className="back-btn"
+                                    onClick={() => navigate("/api/auth/login")}
+                                >
+                                    Back To Login
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ToastContainer />
+        </AuthLayout>
+    );
 }
 
 export default ForgetPassword;
