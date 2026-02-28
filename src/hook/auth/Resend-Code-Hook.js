f5673fbc887221309d5900e950891454d91a0f9b
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-28 06:47:26 +0200

    feat: new hook for sending email again

--- src/hook/auth/Resend-Code-Hook.js
+++ src/hook/auth/Resend-Code-Hook.js
@@ -1,35 +1,53 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { forgetPassword } from "../../redux/actions/authAction";
-import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
-
+import { useNavigate } from "react-router";
 const ResendCodeHook = () => {
-  const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const onResendCode = async () => {
+        const email = localStorage.getItem("user-email");
 
-  const onResendCode = async () => {
-    const email = localStorage.getItem("user-email");
+        if (!email) {
+            toast("Email not found, try logging in again", {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                theme: "light",
+                style: { backgroundColor: "white", color: "#0b236c" },
+            });
+            return;
+        }
 
-    if (!email) {
-      toast("Email not found");
-      return;
-    }
+        setLoading(true);
 
-    setLoading(true);
+        await dispatch(
+            forgetPassword({
+                email,
+            }),
+        );
 
-    await dispatch(
-      forgetPassword({
-        email,
-      })
-    );
+        setLoading(false);
 
-    setLoading(false);
+        toast("Verification code resent to your email", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+            style: { backgroundColor: "white", color: "#0b236c" },
+        });
+        navigate("/api/auth/verify-reset-code");
+    };
 
-    toast("Verification code resent to your email");
-  };
-
-  return [onResendCode, loading];
+    return [onResendCode, loading];
 };
 
 export default ResendCodeHook;
