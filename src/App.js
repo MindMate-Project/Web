04b6136cccae1c2185585629291a47c00c01a592
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-28 06:22:48 +0200

    fix(routes): fix dashboard routes and remove unimplemented routes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import LoginPage from "./pages/Auth/LoginPage";
 import SignupPage from "./pages/Auth/SignupPage";
 import ForgetPasswordPage from "./pages/Auth/ForgetPasswordPage";
 import SetNewPasswordPage from "./pages/Auth/SetNewPasswordPage";
-import VerifyEmailPage from "./pages/Auth/VerifyEmailPage";
-import CheckInboxPage from "./pages/Auth/CheckInboxPage";
+import VerifyResetCodePage from "./pages/Auth/VerifyResetCodePage";
 import VerificationSuccessPage from "./pages/Auth/VerificationSuccessPage";
 import VerificationErrorPage from "./pages/Auth/VerificationErrorPage";
-import PatientsPage from "./pages/Dashboard/PatientsPage";
-import RemindersPage from "./pages/Dashboard/RemindersPage";
-import LiveLocationPage from "./pages/Dashboard/LiveLocationPage";
-import MemoryBankPage from "./pages/Dashboard/MemoryBankPage";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      {/* <LogoHeader/> */}
         <Routes>
           <Route path="/" element={<div>
         <div style={{ display: "flex", gap: "1rem", justifyContent: "center", alignItems: "center", height: "100vh" }}>
@@ -26,17 +21,12 @@ function App() {
         </div>
           </div>} />
 
+          <Route path="/api/dashboard" element={<HomePage />} />
           <Route path="/api/auth/login" element={<LoginPage />} />
           <Route path="/api/auth/signup" element={<SignupPage />} />
           <Route path="/api/auth/forgot-password" element={<ForgetPasswordPage />} />
-          <Route path="/api/auth/verify-reset-password" element={<VerifyEmailPage />} />
+          <Route path="/api/auth/verify-reset-password" element={<VerifyResetCodePage />} />
           <Route path="/api/auth/set-new-password" element={<SetNewPasswordPage />} />
-          <Route path="/api/auth/verify-reset-password" element={<CheckInboxPage />} />
-          <Route path="/api/dashboard/patients" element={<PatientsPage />} />
-          <Route path="/api/dashboard/reminders" element={<RemindersPage />} />
-          <Route path="/api/dashboard/memory-bank" element={<MemoryBankPage />} />
-          <Route path="/api/dashboard/live-location" element={<LiveLocationPage />} />
-          <Route path="/api/dashboard" element={<HomePage />} />
           <Route path="/api/auth/verification-success" element={<VerificationSuccessPage />} />
           <Route path="/api/auth/verification-error" element={<VerificationErrorPage />} />
         </Routes>
