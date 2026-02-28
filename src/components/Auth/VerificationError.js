3dee764164b899b6f751e9676c0d99789758273d
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-28 06:32:59 +0200

    fix: add the logo the jsx

--- src/components/Auth/VerificationError.js
+++ src/components/Auth/VerificationError.js
@@ -1,44 +1,42 @@
 import "./VerificationStatus.css";
 import { useNavigate } from "react-router-dom";
 import { MdError } from "react-icons/md";
-import AuthLayout from "../../components/Auth/AuthLayout";
 import ResendCodeHook from "../../hook/auth/Resend-Code-Hook";
-
+import logo from "../../images/logo.png";
 export default function VerificationErrorPage() {
-  const [onResendCode] = ResendCodeHook();
-  const navigate = useNavigate();
-  
+    const [onResendCode] = ResendCodeHook();
+    const navigate = useNavigate();
 
-  return (
-    <AuthLayout>
-      <div className="verification-page">
-        <div className="verification-card error">
+    return (
+        <div className="verification-page">
+            <div className="logo-header">
+                <img src={logo} alt="Logo" />
+                <h1 className="brand-title">MindMate</h1>
+            </div>
+            <div className="verification-card error">
+                <div className="icon error-icon">
+                    <MdError />
+                </div>
 
-          <div className="icon error-icon">
-            <MdError />
-          </div>
+                <h2 className="error-text">Invalid Code</h2>
 
-          <h2 className="error-text">Invalid Code</h2>
+                <p>
+                    The code you entered is incorrect.
+                    <br />
+                    Please try again or request a new code.
+                </p>
 
-          <p>
-            The code you entered is incorrect.
-            <br />
-            Please try again or request a new code.
-          </p>
+                <button className="primary-btn" onClick={onResendCode}>
+                    Resend Code
+                </button>
 
-          <button className="primary-btn" onClick={onResendCode} >
-            Resend Code
-          </button>
-
-          <button
-            className="secondary-btn"
-            onClick={() => navigate("/api/auth/forgot-password")}
-          >
-            Edit Email
-          </button>
-
+                <button
+                    className="secondary-btn"
+                    onClick={() => navigate("/api/auth/forgot-password")}
+                >
+                    Edit Email
+                </button>
+            </div>
         </div>
-      </div>
-    </AuthLayout>
-  );
+    );
 }
--- src/components/Auth/VerificationSuccess.js
+++ src/components/Auth/VerificationSuccess.js
@@ -1,35 +1,35 @@
 import "./VerificationStatus.css";
 import { useNavigate } from "react-router-dom";
 import { MdCheckCircle } from "react-icons/md";
-import AuthLayout from "../../components/Auth/AuthLayout";
+import logo from "../../images/logo.png";
 
 export default function VerificationSuccessPage() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <AuthLayout>
-      <div className="verification-page">
-        <div className="verification-card success">
+    return (
+        <div className="verification-page">
+            <div className="logo-header">
+                <img src={logo} alt="Logo" />
+                <h1 className="brand-title">MindMate</h1>
+            </div>
+            <div className="verification-card success">
+                <div className="icon success-icon">
+                    <MdCheckCircle />
+                </div>
 
-          <div className="icon success-icon">
-            <MdCheckCircle />
-          </div>
+                <h2 className="success-text">Verification Successful</h2>
 
-          <h2 className="success-text">Verification Successful</h2>
-
-          <p class="verification-text">
-            <span>Your email has been successfully verified.</span>
-            <span>You can now reset your password.</span>
-          </p>
-          <button
-            className="primary-btn"
-            onClick={() => navigate("/api/auth/set-new-password")}
-          >
-            Continue
-          </button>
-
+                <p className="verification-text">
+                    <span>Your email has been successfully verified.</span>
+                    <span>You can now reset your password.</span>
+                </p>
+                <button
+                    className="primary-btn"
+                    onClick={() => navigate("/api/auth/set-new-password")}
+                >
+                    Continue
+                </button>
+            </div>
         </div>
-      </div>
-    </AuthLayout>
-  );
+    );
 }
