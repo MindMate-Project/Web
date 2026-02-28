f0acab7fefd3eb019e29bc7c9f535244e73c500b
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-28 06:24:19 +0200

    fix: remove the old design

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,33 +1,28 @@
 import Dashboard from "./../../components/Dashboard/Dashboard";
-import DashboardLayout from "./../../components/Dashboard/DashboardLayout";
 import { useNavigate } from "react-router";
 
 function HomePage() {
-     const navigate = useNavigate();
-     const logOut = () => {
-         if (localStorage.getItem("user") || localStorage.getItem("token")) {
-             localStorage.removeItem("user");
-             localStorage.removeItem("token");
-         }
-         navigate("/api/auth/login");
-     };
-     const storedUser = localStorage.getItem("user");
-     let user = null;
+    const navigate = useNavigate();
+    const logOut = () => {
+        if (localStorage.getItem("user") || localStorage.getItem("token")) {
+            localStorage.removeItem("user");
+            localStorage.removeItem("token");
+        }
+        navigate("/api/auth/login");
+    };
+    const storedUser = localStorage.getItem("user");
+    let user = null;
 
-     if (storedUser) {
-         try {
-             user = JSON.parse(storedUser);
-         } catch (error) {
-             user = null;
-         }
-     }
+    if (storedUser) {
+        try {
+            user = JSON.parse(storedUser);
+        } catch (error) {
+            user = null;
+        }
+    }
 
-     const userName = user && user.name ? user.name : "Guest";
-     return (
-        <DashboardLayout userName={userName} logOut={logOut}>
-            <Dashboard />
-        </DashboardLayout>
-  );
+    const userName = user && user.name ? user.name : "Guest";
+    return <Dashboard userName={userName} logOut={logOut} />;
 }
 
 export default HomePage;
