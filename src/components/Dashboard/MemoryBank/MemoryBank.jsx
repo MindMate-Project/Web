570def13a37251827a15e943804b7c5ac5411638
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-03-02 01:30:53 +0200

    refactor: new folder for memory bank dashboard

---
+++ src/components/Dashboard/MemoryBank/MemoryBank.jsx
@@ -0,0 +1,8 @@
+export default function MemoryBank() {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Memory Bank Page</h1>
+            <p>This is where you can manage your memory bank.</p>
+        </div>
+    );
+}
