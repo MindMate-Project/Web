546e2a58e8f9fd6c12f386e86b5c014c01a0ed2a
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-03-01 06:30:39 +0200

    feat(Reminders): new feature structure of setting reminders

---
+++ src/components/Dashboard/Reminders.jsx
@@ -0,0 +1,8 @@
+export default function Reminders() {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Reminders Page</h1>
+            <p>This is where you can manage your reminders.</p>
+        </div>
+    );
+}
