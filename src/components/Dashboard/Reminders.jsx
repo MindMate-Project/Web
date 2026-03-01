893253a22fa6965b61133e3af44223373304dc49
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-03-02 01:30:06 +0200

    refactor: move this folder to his folder

--- src/components/Dashboard/Reminders.jsx
+++
@@ -1,8 +0,0 @@
-export default function Reminders() {
-    return (
-        <div style={{ padding: "2rem" }}>
-            <h1>Reminders Page</h1>
-            <p>This is where you can manage your reminders.</p>
-        </div>
-    );
-}
