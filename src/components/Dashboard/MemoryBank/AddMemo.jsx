e858bc14a87904e23033115c8dd0564d2db17924
Alaa Mohamed <allaamohmed152@gmail.com>
2026-03-08 02:06:25 +0200

    feat: update AddMemo component

--- src/components/Dashboard/MemoryBank/AddMemo.jsx
+++ src/components/Dashboard/MemoryBank/AddMemo.jsx
@@ -3,20 +3,20 @@ import "./AddMemo.css";
 
 function AddMemory() {
   return (
-    <div className="addMemoryContainer">
+    <div className="addmemory-container">
 
-      <p className="title">Add New Memory</p>
+      <p className="addmemory-title">Add New Memory</p>
 
-      <div className="memoryCard">
+      <div className="addmemory-card">
 
-        <div className="formSection">
+        <div className="addmemory-form-section">
 
           {/* Upload */}
-          <div className="uploadBox">
+          <div className="addmemory-upload-box">
             <input type="file" id="fileUpload" hidden />
             <label htmlFor="fileUpload">
-              <div className="uploadContent">
-                <div className="uploadIcon">
+              <div className="addmemory-upload-content">
+                <div className="addmemory-upload-icon">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M352 173.3L352 384C352 401.7 337.7 416 320 416C302.3 416 288 401.7 288 384L288 173.3L246.6 214.7C234.1 227.2 213.8 227.2 201.3 214.7C188.8 202.2 188.8 181.9 201.3 169.4L297.3 73.4C309.8 60.9 330.1 60.9 342.6 73.4L438.6 169.4C451.1 181.9 451.1 202.2 438.6 214.7C426.1 227.2 405.8 227.2 393.3 214.7L352 173.3zM320 464C364.2 464 400 428.2 400 384L480 384C515.3 384 544 412.7 544 448L544 480C544 515.3 515.3 544 480 544L160 544C124.7 544 96 515.3 96 480L96 448C96 412.7 124.7 384 160 384L240 384C240 428.2 275.8 464 320 464zM464 488C477.3 488 488 477.3 488 464C488 450.7 477.3 440 464 440C450.7 440 440 450.7 440 464C440 477.3 450.7 488 464 488z"/></svg>
                 </div>
                 <p>Upload Media</p>
@@ -25,33 +25,32 @@ function AddMemory() {
           </div>
 
           {/* Select */}
-          <div className="memoryForm">
-            <select className="memorySelect">
+          <div className="addmemory-form">
+            <select className="addmemory-memory-select">
               <option>Choose Memory Type</option>
               <option>Photo</option>
               <option>Video</option>
               <option>Text</option>
             </select>
 
-            <div className="inputGroup">
+            <div className="addmemory-input-group">
                 <label>Title</label>
                 <input type="text" />
             </div>
 
-            <div className="inputGroup">
+            <div className="addmemory-input-group">
               <label>Caption</label>
               <textarea></textarea>
             </div>
 
-            <div className="inputGroup">
+            <div className="addmemory-input-group">
               <label>Date (Optional)</label>
               <input type="date" />
-
-              
             </div>
-            <div className="buttons">
-                  <button className="saveBtn">Save</button>
-                  <button className="cancelBtn">Cancel</button>
+
+            <div className="addmemory-buttons">
+              <button className="addmemory-save-btn">Save</button>
+              <button className="addmemory-cancel-btn">Cancel</button>
             </div>
 
           </div>
