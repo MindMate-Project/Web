3378fe90be712c5af996aa566231a0cba9b76cb2
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-03-11 01:26:55 +0200

    feat(patient): integrate with patient reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
-
+import patientReducer from "../slices/patientSlice";
 export default combineReducers({
     authReducer: authReducer,
+    patientReducer: patientReducer,
 });
