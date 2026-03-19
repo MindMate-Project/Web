49a8a2b2e678b125538dbe8e9615907d9b4cb140
Alaa Mohamed <allaamohmed152@gmail.com>
2026-03-19 04:58:13 +0200

    chore: import reminderSlice in rootReducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
 import patientReducer from "../slices/patientSlice";
+import reminderReducer from "../slices/reminderSlice";
 export default combineReducers({
     authReducer: authReducer,
     patientReducer: patientReducer,
+    reminderReducer: reminderReducer,
 });
