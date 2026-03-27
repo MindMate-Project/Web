502e160463996bf38dacc732cbd17322d345f37e
Alaa Mohamed <allaamohmed152@gmail.com>
2026-03-27 02:19:45 +0200

    feat: update getRemindersHook logic

--- src/hook/reminder/getRemindersHook.js
+++ src/hook/reminder/getRemindersHook.js
@@ -4,14 +4,16 @@ import { getRemindersByPatient } from "../../redux/slices/reminderSlice";
 
 const useGetReminders = () => {
   const dispatch = useDispatch();
-  const reminders = useSelector(state => state.reminder.reminders);
+  const reminders = useSelector(state => state.reminderReducer?.reminders || []);
   const patientId = localStorage.getItem("patientId");
 
   useEffect(() => {
-    if (patientId) dispatch(getRemindersByPatient(patientId));
+    if (patientId) {
+      dispatch(getRemindersByPatient(patientId));
+    }
   }, [dispatch, patientId]);
 
-  return reminders;
+  return { reminders, getReminders: (id) => dispatch(getRemindersByPatient(id)) };
 };
 
 export default useGetReminders;
