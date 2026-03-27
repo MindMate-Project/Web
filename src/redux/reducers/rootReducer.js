a653353a3c08273b408eb5bbeeb8dd222433792a
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-03-27 14:33:07 +0200

    feat(memory): integrate memory slice into root reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
 import patientReducer from "../slices/patientSlice";
 import reminderReducer from "../slices/reminderSlice";
+import memoryReducer from "../slices/memorySlice";
 export default combineReducers({
     authReducer: authReducer,
     patientReducer: patientReducer,
     reminderReducer: reminderReducer,
+    memoryReducer: memoryReducer,
 });
