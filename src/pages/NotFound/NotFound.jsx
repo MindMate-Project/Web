268ac82e9d840c36b087f647a8fb5d914c9d1da6
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-06-16 00:51:08 +0300

    update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../../images/logo.png";
+import logo from "../../images/logo.webp";
 
 const NotFound = () => {
     const navigate = useNavigate();
