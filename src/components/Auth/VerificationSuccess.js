485b38ccc2cd0b4459e7d177108915880bd27783
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-06-16 00:51:07 +0300

    update VerificationSuccess.js

--- src/components/Auth/VerificationSuccess.js
+++ src/components/Auth/VerificationSuccess.js
@@ -1,34 +1,33 @@
 import "./VerificationStatus.css";
 import { useNavigate } from "react-router-dom";
 import { MdCheckCircle } from "react-icons/md";
-import logo from "../../images/logo.png";
+import AuthNavbar from "../AuthNavbar/AuthNavbar";
 
 export default function VerificationSuccessPage() {
     const navigate = useNavigate();
 
     return (
         <div className="verification-page">
-            <div className="logo-header">
-                <img src={logo} alt="Logo" />
-                <h1 className="brand-title">MindMate</h1>
-            </div>
-            <div className="verification-card success">
-                <div className="icon success-icon">
-                    <MdCheckCircle />
+            <AuthNavbar />
+            <div className="container">
+                <div className="verification-card success">
+                    <div className="icon success-icon">
+                        <MdCheckCircle />
+                    </div>
+
+                    <h2 className="success-text">Verification Successful</h2>
+
+                    <p className="verification-text">
+                        <span>Your email has been successfully verified.</span>
+                        <span>You can now reset your password.</span>
+                    </p>
+                    <button
+                        className="primary-btn"
+                        onClick={() => navigate("/api/auth/set-new-password")}
+                    >
+                        Continue
+                    </button>
                 </div>
-
-                <h2 className="success-text">Verification Successful</h2>
-
-                <p className="verification-text">
-                    <span>Your email has been successfully verified.</span>
-                    <span>You can now reset your password.</span>
-                </p>
-                <button
-                    className="primary-btn"
-                    onClick={() => navigate("/api/auth/set-new-password")}
-                >
-                    Continue
-                </button>
             </div>
         </div>
     );
